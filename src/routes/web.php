378b736f657d0646a4c08cc76e289e2174a46158
misa <mmichiura33@gmail.com>
2025-06-03 15:39:30 +0900

    サンクスページのビューとCSSを作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::post('/register', [AuthController::class, 'store'])
+    ->middleware(['guest'])
+    ->name('register');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/thanks', 'auth.thanks')->name('register.thanks');
