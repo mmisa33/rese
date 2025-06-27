6cf3a6df5c8262bdca31ff68b595efd88597842f
misa <mmichiura33@gmail.com>
2025-06-27 14:50:57 +0900

    CustomLoginResponseを修正

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('user'); // 'admin', 'owner', 'user'
+            $table->string('role')->default('user'); // 'user', 'admin', 'owner'
             $table->rememberToken();
             $table->timestamps();
         });
